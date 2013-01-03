bcdc049ee1c537de317fb3dc9ee3c02157ab76d2
Stuart Moore <stuart@informationarchitects.jp>
2013-01-03 16:52:43 +0100

    Add enclosure.

---
+++ TWiT.tv/Enclosure.h
@@ -0,0 +1,24 @@
+//
+//  Enclosure.h
+//  TWiT.tv
+//
+//  Created by Stuart Moore on 1/3/13.
+//  Copyright (c) 2013 Stuart Moore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+#import "Feed.h"
+
+@class Episode;
+
+@interface Enclosure : NSManagedObject
+
+@property (nonatomic, strong) NSString *title, *subtitle;
+@property (nonatomic, strong) NSString *url, *path;
+@property (nonatomic) TWQuality quality;
+@property (nonatomic) TWType type;
+@property (nonatomic, strong) Episode *episode;
+
+@end
---
+++ TWiT.tv/Enclosure.m
@@ -0,0 +1,16 @@
+//
+//  Enclosure.m
+//  TWiT.tv
+//
+//  Created by Stuart Moore on 1/3/13.
+//  Copyright (c) 2013 Stuart Moore. All rights reserved.
+//
+
+#import "Enclosure.h"
+#import "Episode.h"
+
+@implementation Enclosure
+
+@dynamic path, quality, subtitle, title, type, url, episode;
+
+@end
