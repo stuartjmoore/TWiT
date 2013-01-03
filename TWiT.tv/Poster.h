9efd9ec510fa6de9d17b685d2b0dd0067c3a94bf
Stuart Moore <stuart@informationarchitects.jp>
2013-01-03 16:54:37 +0100

    Add Poster Object.

---
+++ TWiT.tv/Poster.h
@@ -0,0 +1,19 @@
+//
+//  Poster.h
+//  TWiT.tv
+//
+//  Created by Stuart Moore on 1/3/13.
+//  Copyright (c) 2013 Stuart Moore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class Episode;
+
+@interface Poster : NSManagedObject
+
+@property (nonatomic, retain) NSString *url, *path;
+@property (nonatomic, retain) Episode *episode;
+
+@end
---
+++ TWiT.tv/Poster.m
@@ -0,0 +1,23 @@
+//
+//  Poster.m
+//  TWiT.tv
+//
+//  Created by Stuart Moore on 1/3/13.
+//  Copyright (c) 2013 Stuart Moore. All rights reserved.
+//
+
+#import "Poster.h"
+#import "Episode.h"
+
+
+@implementation Poster
+
+@dynamic path, url, episode;
+
+- (void)prepareForDeletion
+{
+    if([NSFileManager.defaultManager fileExistsAtPath:self.path])
+        [NSFileManager.defaultManager removeItemAtPath:self.path error:nil];
+}
+
+@end
