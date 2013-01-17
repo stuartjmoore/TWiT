aaa6cebdbfd9e8b3775863b6803938eb5bb031c4
Stuart Moore <stuart@informationarchitects.jp>
2013-01-17 11:55:31 -0500

    Only update fav shows. Doh.

--- TWiT.tv/TWAppDelegate.m
+++ TWiT.tv/TWAppDelegate.m
@@ -85,7 +85,8 @@
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     
     for(Show *show in self.channel.shows)
-        [show updateEpisodes];
+        if(show.favorite)
+            [show updateEpisodes];
     
     if(self.channel.schedule.days.count > 0)
     {
