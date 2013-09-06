7cba3d692407018f563e56afa71b6fd26f3d5c27
stuartjmoore <stuartjmoore@gmail.com>
2013-09-06 10:18:00 -0400

    Only a temp fix.

--- TWiT.tv/main.m
+++ TWiT.tv/main.m
@@ -1,3 +1,32 @@
+#import <UIKit/UIKit.h>
+#import "TWAppDelegate.h"
+
+typedef int (*PYStdWriter)(void *, const char *, int);
+static PYStdWriter _oldStdWrite;
+
+int __pyStderrWrite(void *inFD, const char *buffer, int size)
+{
+    if ( strncmp(buffer, "AssertMacros:", 13) == 0 )
+    {
+        return 0;
+    }
+    return _oldStdWrite(inFD, buffer, size);
+}
+
+int main(int argc, char * argv[])
+{
+    _oldStdWrite = stderr->_write;
+    stderr->_write = __pyStderrWrite;
+    @autoreleasepool
+    {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWAppDelegate class]));
+    }
+}
+
+/*
+ 
+ TODO: Remove, only a temp fix for the crazy NSLog error.
+ 
 //
 //  main.m
 //  TWiT.tv
@@ -16,3 +45,5 @@ int main(int argc, char *argv[])
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWAppDelegate class]));
     }
 }
+ 
+*/
